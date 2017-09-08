a09ed49a69774c93103627e3c3d5cbff23c44b74
Jonathan Berry <jonathan.s.berry@googlemail.com>
2017-09-08 11:19:48 +0100

    Debugging init

--- util/repo.py
+++ util/repo.py
@@ -822,6 +822,8 @@ def _SetDefaultsTo(gitdir):
 def main(orig_args):
   _print(str(orig_args))
   _print(str(extra_args))
+  extra_args = []
+
   cmd, opt, args = _ParseArguments(orig_args)
 
   repo_main, rel_repo_dir = None, None
